df00e6b41e57c459e6a4e31673cf570bb4c27169
educlick <educlick518@gmail.com>
2016-10-27 19:48:47 -0300

    Update1

--- modulo-2/practicas/practica-4/team_players.rb
+++ modulo-2/practicas/practica-4/team_players.rb
@@ -1,35 +1,42 @@
 class Player
-  #attr_reader
-  #initialize
-
+  attr_accessor :name, :age, :skill_level
+  def initialize (name,age,skill_level)
+    @name=name
+    @age=age
+    @skill_level=skill_level
+  end
   def to_s
     "<#{name}: #{skill_level}(SL), #{age}(AGE)>"
   end
-
 end
-
 class Team
-  #attr_accessor
-  #initialize
-  #add_players
-
+  include Enumerable
+  #elig_players = []
+  attr_accessor :name
+  players = [];
+  def initialize (name)
+    @name=name
+  end
+  def add_players (*player)
+    @players=player
+  end
   def to_s
     "#{@name} team: #{@players.join(", ")}"
   end
-
+  def each
+    @players.each { |player| yield player }
+  end
 end
-
 player1 = Player.new("Bob", 13, 5)
 player2 = Player.new("Jim", 15, 4.5)
 player3 = Player.new("Mike", 21, 5)
 player4 = Player.new("Joe", 14, 5)
 player5 = Player.new("Scott", 16, 3)
-
 red_team = Team.new("Red")
 red_team.add_players(player1, player2, player3, player4, player5)
-
 # Seleccionar únicamente los jugadores entre 14 y 20 años y no incluir a ningún jugador con skill-level menor a 4.5
 elig_players = []#COLOCAR AQUÍ LA SELECCION#
+elig_players = red_team.select {|team| team.age >= 14 and team.age <= 20 and team.skill_level >= 4.5}
 puts elig_players
 # DEBE IMPRIMIR
 # => <Jim: 4.5(SL), 15(AGE)>
